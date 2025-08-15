1ce58e5e4b6e892e54e3d00d02c6783eb0f43e0e
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-15 06:23:46 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Team Schedule Manager</h1>
+      <p>Collaboratively manage your team's schedule in real time!</p>
+    </div>
+  );
+}
+
+export default App;
